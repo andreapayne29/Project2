02cb3dfc8206f30bd9001b75bb1772fdfe495f19
Andrea Payne <andiepayne@outlook.com>
2021-04-12 13:20:11 -0400

    updated LDA, removed uninformative analysis. added gamma analysis to individual questions

--- Full LDA Analysis Including Per Question.R
+++ Full LDA Analysis Including Per Question.R
@@ -9,15 +9,15 @@ library(topicmodels)
 library(scales)
 
 
+
 ########### FUNCTIONS ##############
 
 #### cleaning_function ####
 ## input: full response data
 ## output: responses to a single question and uniquely numbered
 cleaning_function <- function(data){
-  data_cleaned = data %>% select(question, english) %>% 
-    transmute(document = question, text = english) %>% 
-    mutate(document = 1:663)
+  data_cleaned = data %>% select(c_1, question, english) %>% 
+    transmute(document = c_1, text = english)
 }
 
 
@@ -47,9 +47,9 @@ split_by_words_function <- function(df){
 sparsity_correction_function <- function(dtm, sparcity = 0.95, word_counts){
   DTM_sc = dtm #was removeSparseTerms(dtm, sparcity) but that messes up any data
   DTM_sc_matrix = as.matrix(DTM_sc)
-  Removethese = which(apply(DTM_sc_matrix, 1, function(x) {sum(x>0)<5|sum(x)<5}))
+  #Removethese = which(apply(DTM_sc_matrix, 1, function(x) {sum(x>0)<5|sum(x)<5}))
   
-  FullDTM = word_counts %>% filter(!(word_counts$document %in% names(Removethese))) %>%
+  FullDTM = word_counts %>% #filter(!(word_counts$document %in% names(Removethese))) %>%
     cast_dtm(document, word, n)
   
   return(FullDTM)
@@ -85,16 +85,17 @@ LDA_function <- function(kmin=2, kmax, testingDTM, trainingDTM, questionNumber){
     LDAout[[k]] = LDA(trainingDTM, k+1)
     perpl = rbind(perpl, c(k+1, perplexity(LDAout[[k]], newdata = testingDTM, control = list(seed = 29))))
   }
-  plot(perpl[,1], perpl[,2], main = paste0("Perplexity, ", questionNumber))
+  plot(perpl[,1], perpl[,2], main = paste0("Perplexity, Question ", questionNumber))
   idealK = which.min(perpl[,2])
-  return(LDAout[[idealK]])
+  return(idealK+1)
   
 }
 
 #### topic_plotting_function ####
 ## input: a beta matrix from LDA analysis
 ## output: topics plot
-topic_plotting_function <- function(betaMatrix, n){
+topic_plotting_function <- function(LDA, n, questionNumber){
+  betaMatrix = tidy(LDA, matrix = "beta")
   betaMatrix %>%
     group_by(topic) %>%
     top_n(n, beta) %>%
@@ -105,7 +106,8 @@ topic_plotting_function <- function(betaMatrix, n){
     geom_col(show.legend = FALSE)+
     facet_wrap(~ topic, scales = "free") + 
     coord_flip()+
-    scale_x_reordered()
+    scale_x_reordered()+
+    ggtitle(paste0("Topics for Question ", questionNumber))
 }
 
 
@@ -117,8 +119,8 @@ topic_plotting_function <- function(betaMatrix, n){
 ##        number of terms to plot
 ## Output: LDA perplexity plot and topic plot from ideal k LDA analysis
 
-individual_question_LDA <- function(questionNumber, original_data = survey_data, kmin=2, kmax=10, testingSplit=0.9, n=10){
-  # aquiring data
+individual_question_LDA <- function(questionNumber, original_data = survey_data, kmin=2, kmax=10, testingSplit=0.9){
+  # acquiring data
   data = original_data %>% filter(question == paste0("Question ", questionNumber))
   # cleaning
   cleaned = cleaning_function(data)
@@ -132,15 +134,39 @@ individual_question_LDA <- function(questionNumber, original_data = survey_data,
   DTMTrain = FullDTM[-TestSet,]
   
   # running LDA
-  LDA = LDA_function(kmin = kmin, kmax = kmax, testingDTM = DTMTest, trainingDTM = DTMTrain, questionNumber = questionNumber)
-  # creating beta matrix
-  LDATopics = tidy(LDA, matrix = "beta")
-  # plotting
-  topic_plotting_function(LDATopics, n = n)
-  
+  k = LDA_function(kmin = kmin, kmax = kmax, testingDTM = DTMTest, trainingDTM = DTMTrain, questionNumber = questionNumber)
+  LDA = LDA(FullDTM, k)
+  return(LDA)
 }
 
 
+##### classify_responses #####
+### Input: LDA
+### Output: 3 col tibble with response ID, associated topic, and gamma value
+classify_responses <- function(LDA){
+  #creating a gamma matrix
+  LDA_gamma = tidy(LDA, matrix = "gamma")
+  ## determining which answer assigns to what topic
+  question_classification <- LDA_gamma %>%
+    group_by(document) %>%
+    slice_max(gamma) %>%
+    ungroup()
+  
+  return(question_classification)
+}
+
+##### determine_missing_ans #####
+#### Due to cleaning, some responses are lost. This is to manually check the missing answers
+#### to see if any answer of value is lost (i.e. anything more informative that "no"/"n/a" etc.)
+## Input: question classification tibble, question number, and cleaned survey_data
+## Output: response IDs that are not present in topic analysis
+determine_missing_ans <- function(question_classification, questionNumber, survey_data){
+  question_filter = as.character(paste0("Question ", questionNumber))
+  final_responses = question_classification$document
+  actual_responses = survey_data  %>%filter(question == question_filter)
+  missing_values = setdiff(actual_responses$c_1, final_responses)
+  return(missing_values)                                  
+}
 
 
 
@@ -148,7 +174,7 @@ individual_question_LDA <- function(questionNumber, original_data = survey_data,
 
 
 # loading in data
-survey_data = readxl::read_xlsx() #insert file path here
+survey_data = readxl::read_xlsx("masterResponse.xlsx")
 # creating a vector to identify what question is being answered
 questionsCol = rep(1:9, len = nrow(survey_data))
 # adding question id
@@ -157,120 +183,81 @@ survey_data = survey_data %>% mutate(question = paste0("Question ", questionsCol
 
 
 
-#### Total LDA Analysis ####
 
-#cleaning full survey answers to only have question id (document) and response (text)
-cleaned_df = survey_data %>% select(question, english) %>% transmute(document = question, text = english)
-#removing NAs and breaking into words
-FullDTM = LDA_prep_function(cleaned_df)
+##### Individual Question LDA #####
 
+question1 = individual_question_LDA(1)
+topic_plotting_function(question1, n = 10, questionNumber = 1)
+question1 = classify_responses(question1)
 
-##### 'Ideal' Number of Topics #####
-TestSet = sort(sample(1:nrow(FullDTM), floor(nrow(FullDTM)*.70)))
-DTMTest = FullDTM[TestSet,]
-DTMTrain = FullDTM[-TestSet,]
-LDA = LDA_function(kmin = 2, kmax = 25, testingDTM = DTMTest, trainingDTM = DTMTrain, questionNumber = "Total")
-# creating beta matrix
-LDATopics = tidy(LDA, matrix = "beta")
-# plotting
-topic_plotting_function(LDATopics, n = 10)
+question2 = individual_question_LDA(2)
+topic_plotting_function(question2, n = 10, questionNumber = 2)
+question2 = classify_responses(question2)
 
+question3 = individual_question_LDA(3)
+topic_plotting_function(question3, n = 10, questionNumber = 3)
+question3 = classify_responses(question3)
 
+question4 = individual_question_LDA(4)
+topic_plotting_function(question4, n = 10, questionNumber = 4)
+question4 = classify_responses(question4)
 
-##### 9 Topic LDA #####
+question5 = individual_question_LDA(5)
+topic_plotting_function(question5, n = 10, questionNumber = 5)
+question5 = classify_responses(question5)
 
+question6 = individual_question_LDA(6)
+topic_plotting_function(question6, n = 10, questionNumber = 6)
+question6 = classify_responses(question6)
 
-## All gamma visualization and analysis code comes from Chapter 6 of the textbook "Test Mining with R: A Tidy Approach" by Julia Silge and David Robinson
-## https://www.tidytextmining.com/index.html
+question7 = individual_question_LDA(7)
+topic_plotting_function(question7, n = 10, questionNumber = 7)
+question7 = classify_responses(question7)
 
-#running LDA
-LDA_9_topics = LDA(FullDTM, k = 9, control = list(seed = 29))
+question8 = individual_question_LDA(8)
+topic_plotting_function(question8, n = 10, questionNumber = 8)
+question8 = classify_responses(question8)
 
-#creating beta matrix for total document analysis
-Topics = tidy(LDA_9_topics, matrix = "beta")
-#creating gamma matrix for individual document analysis
-LDA_gamma = tidy(LDA_9_topics, matrix = "gamma")
-
-
-## determining which question assigns to what topic
-## 3 col tibble with document, topic, and gamma value
-question_classification <- LDA_gamma %>%
-  group_by(document) %>%
-  slice_max(gamma) %>%
-  ungroup()
-
-## 2 col tibble with document and topic
-question_topics <- question_classification %>%
-  count(document, topic) %>%
-  group_by(document) %>% 
-  slice_max(n, n= 1) %>%
-  ungroup() %>%
-  transmute(consensus = document, topic)
+question9 = individual_question_LDA(9)
+topic_plotting_function(question9, n = 10, questionNumber = 9)
+question9 = classify_responses(question9)
 
 
-## Visualization
-## top words per topic plot
-topic_plotting_function(Topics, n = 10)
-# related questions to topic for reference
-print(question_topics)
-
-
-## gamma correspondence with topic to question
-LDA_gamma %>%
-  mutate(title = reorder(document, gamma * topic)) %>%
-  ggplot(aes(factor(topic), gamma)) +
-  geom_boxplot() +
-  facet_wrap(~ document) +
-  labs(x = "topic", y = expression(gamma))
-
-## topics that were misclassified
-question_classification %>%
-  inner_join(question_topics, by = "topic") %>%
-  filter(document != consensus)
-
-## determining which words were assigned to which topic
-assignments <- augment(LDA_9_topics, data = FullDTM)
-
-## adding actual correspondence
-assignments = assignments %>%
-  inner_join(question_topics, by = c(".topic" = "topic"))
-
-
-## a different way of visualizing misclassifications
-assignments %>%
-  count(document, consensus, wt = count) %>%
-  mutate(across(c(document, consensus), ~str_wrap(., 20))) %>%
-  group_by(document) %>%
-  mutate(percent = n / sum(n)) %>%
-  ggplot(aes(consensus, document, fill = percent)) +
-  geom_tile() +
-  scale_fill_gradient2(high = "darkred", label = percent_format()) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1),
-        panel.grid = element_blank()) +
-  labs(x = "Question words were assigned to",
-       y = "Question words came from",
-       fill = "% of assignments")
-
-## words that were misclassified and where
-wrong_words <- assignments %>%
-  filter(document != consensus)
-wrong_words
+## writing CSVs for further analysis with response ID/topic association
+write.csv(question1,file="Question 1 Response Topics LDA.csv", row.names = FALSE)
+write.csv(question2,file="Question 2 Response Topics LDA.csv", row.names = FALSE)
+write.csv(question3,file="Question 3 Response Topics LDA.csv", row.names = FALSE)
+write.csv(question4,file="Question 4 Response Topics LDA.csv", row.names = FALSE)
+write.csv(question5,file="Question 5 Response Topics LDA.csv", row.names = FALSE)
+write.csv(question6,file="Question 6 Response Topics LDA.csv", row.names = FALSE)
+write.csv(question7,file="Question 7 Response Topics LDA.csv", row.names = FALSE)
+write.csv(question8,file="Question 8 Response Topics LDA.csv", row.names = FALSE)
+write.csv(question9,file="Question 9 Response Topics LDA.csv", row.names = FALSE)
 
 
 
+#removing NAs from initial dataset
+removed_missing_sd = survey_data %>% remove_missing(vars = "english")
 
+#finding answers that were lost to cleaning (ex. no, n/a, nope, etc.)
+missing1 = determine_missing_ans(question1, 1, removed_missing_sd)
+missing2 = determine_missing_ans(question2, 2, removed_missing_sd)
+missing3 = determine_missing_ans(question3, 3, removed_missing_sd)
+missing4 = determine_missing_ans(question4, 4, removed_missing_sd)
+missing5 = determine_missing_ans(question5, 5, removed_missing_sd)
+missing6 = determine_missing_ans(question6, 6, removed_missing_sd)
+missing7 = determine_missing_ans(question7, 7, removed_missing_sd)
+missing8 = determine_missing_ans(question8, 8, removed_missing_sd)
+missing9 = determine_missing_ans(question9, 9, removed_missing_sd)
 
+write.csv(missing1,file="Question 1 Missing Responses.csv", row.names = FALSE)
+write.csv(missing2,file="Question 2 Missing Responses.csv", row.names = FALSE)
+write.csv(missing3,file="Question 3 Missing Responses.csv", row.names = FALSE)
+write.csv(missing4,file="Question 4 Missing Responses.csv", row.names = FALSE)
+write.csv(missing5,file="Question 5 Missing Responses.csv", row.names = FALSE)
+write.csv(missing6,file="Question 6 Missing Responses.csv", row.names = FALSE)
+write.csv(missing7,file="Question 7 Missing Responses.csv", row.names = FALSE)
+write.csv(missing8,file="Question 8 Missing Responses.csv", row.names = FALSE)
+write.csv(missing9,file="Question 9 Missing Responses.csv", row.names = FALSE)
 
-##### Individual Quesiton LDA #####
-
-individual_question_LDA(1)
-individual_question_LDA(2)
-individual_question_LDA(3)
-individual_question_LDA(4)
-individual_question_LDA(5)
-individual_question_LDA(6)
-individual_question_LDA(7)
-individual_question_LDA(8)
-individual_question_LDA(9)
 
